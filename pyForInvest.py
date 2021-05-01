0ba5563b5fc92a7d1164ebcdaeaed2cb27170dfe
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2021-05-01 19:51:35 -0300

    import

---
+++ pyForInvest.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader.data as web
+
+pip install yfinance --upgrade --no-cache-dir
+import yfinance as yf
+yf.pdr_override() 
