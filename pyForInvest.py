45f0915d83e1cd0977d80be7ab071a349157cd62
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2021-05-21 16:15:26 -0300

    mt5 primeiro no vs

--- pyForInvest.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import pandas_datareader.data as web
-
-pip install yfinance --upgrade --no-cache-dir
-import yfinance as yf
-yf.pdr_override() 
